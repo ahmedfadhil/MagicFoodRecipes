4461be77539a76cf42722f43182fefc06deb5d8f
Ahmed fadhil <ahmed.fadhil@alumni.emu.edu.tr>
2015-12-29 12:35:47 +0100

    Production mode and AWS

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,5 +1,7 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.root = Rails.root.join('tmp') # adding these...
+    config.cache_dir = 'carrierwave' # ...two lines
     {
         :provider => 'AWS',
         :aws_access_key_id => ENV['S3_ACCESS_KEY'],
